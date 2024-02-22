5017a3d6849dd638273f01a9f2791b1f93ebefe7
FelipeAP04 <agu23195@uvg.edu.gt>
2024-02-21 20:54:20 -0600

    Junit y Uml

--- hdt4/src/test/java/uvg/edu/gt/ListaTest.java
+++ hdt4/src/test/java/uvg/edu/gt/ListaTest.java
@@ -1,5 +1,10 @@
 package uvg.edu.gt;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 public class ListaTest {
-    
+    @Test
+    public void shouldAnswerWithTrue(){
+        assertTrue(true);
+    }
 }
--- hdt4/src/test/java/uvg/edu/gt/PilaTest.java
+++ hdt4/src/test/java/uvg/edu/gt/PilaTest.java
@@ -1,5 +1,10 @@
 package uvg.edu.gt;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 public class PilaTest {
-    
+    @Test
+    public void shouldAnswerWithTrue(){
+        assertTrue(true);
+    }
 }
