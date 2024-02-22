1a579c9801223980567940e199f3e261a823759c
FelipeAP04 <agu23195@uvg.edu.gt>
2024-02-21 18:11:18 -0600

    correcion de leer datos

--- hdt4/src/main/java/uvg/edu/gt/App.java
+++ hdt4/src/main/java/uvg/edu/gt/App.java
@@ -1,5 +1,10 @@
 package uvg.edu.gt;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App {
     public static void main(String[] args) {
         Pila<Integer> arrayListPila = new ArrayListPila<>();
@@ -10,7 +15,31 @@ public class App {
 
         System.out.println("\nProbando VectorPila:");
         probarPila(vectorPila);
+
+        try {
+            String infixExpression = leerExpresionFromFile("datos.txt");
+            if (infixExpression != null && !infixExpression.isEmpty()) {
+                String postfixExpression = Calculator.convertToPostfix(infixExpression);
+                System.out.println("Expresión Postfix: " + postfixExpression);
+            } else {
+                System.out.println("No se pudo leer la expresión infix del archivo.");
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
     }
+
+    private static String leerExpresionFromFile(String fileName) throws IOException {
+    StringBuilder sb = new StringBuilder();
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(App.class.getClassLoader().getResourceAsStream(fileName)))) {
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+    }
+    return sb.toString();
+}
+
 
     private static void probarPila(Pila<Integer> pila) {
         try {
@@ -27,4 +56,3 @@ public class App {
         }
     }
 }
-
